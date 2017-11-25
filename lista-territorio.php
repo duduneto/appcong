ff481a016dd90fac90a81cf3c28e80821f494bbb
duduneto <31359405+duduneto@users.noreply.github.com>
2017-11-25 01:16:20 -0200

    Add Classe que estiliza Btn

--- lista-territorio.php
+++ lista-territorio.php
@@ -2,11 +2,11 @@
 
 
 <div class="btn btn-toolbar">
-  <?php for ($i=1; $i<16 ; $i++) {?>
+  <?php for ($i=1; $i<27 ; $i++) {?>
     <div class="ml-2 mr-2">
       <form class="" action="cookear-territorio.php" method="post">
         <input type="hidden" name="numero-territorio" value="<?= $i ?>">
-        <button class="btn btn-secondary"type="submit"><?= $i ?></button>
+        <button class="btn btn-secondary largurabtn" type="submit"><?= $i ?></button>
       </form>
     </div>
 

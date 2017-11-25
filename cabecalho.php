1137103ba8ef27013f1892d2ae46d05a5696a906
duduneto <31359405+duduneto@users.noreply.github.com>
2017-11-25 01:15:05 -0200

    css/territorio.css subiu de linha

--- cabecalho.php
+++ cabecalho.php
@@ -18,10 +18,10 @@ verificaUsuario();
     <title>Controle Território</title>
     <link rel="icon" href="favicon.png">
     <link href="css/loja.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/territorio.css">
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <!--<link rel="stylesheet" href="css/jquery.modal.min.css">--><!--SE USAR O QUERY.MODAL vai dar conflito com o Modal do BootStrap-->
-    <link rel="stylesheet" href="/css/territorio.css">
 
   </head>
   <body>

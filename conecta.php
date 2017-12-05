8df786168f77ae479b30b8aa4990fa3f53e41298
duduneto <31359405+duduneto@users.noreply.github.com>
2017-12-05 13:51:23 -0200

    Simplify Code

--- conecta.php
+++ conecta.php
@@ -3,8 +3,5 @@ $host = "localhost";
 $user = "root";
 $senha = "";
 $banco = "territorio";
-$conexao = new mysqli($host, $user, $senha, $banco);
-        if($conex -> connect_errno){
-          die('Falha na conexão: ' . $conex->connect_error);
-        }else print("Conectado.")
+$conexao = mysqli_connect($host,$user,$senha,$banco);
  ?>
